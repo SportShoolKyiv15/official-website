4eb4d0985bc3e2a7b26ff478ace9f027a0cd1e64
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-03-17 18:07:15 +0200

    Add button with double arrow and correct Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,52 +1,85 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Nav: React.FC = () => {
+	const [isToggled, setIsToggled] = useState(false);
+	const onSwitch = () => {
+		setIsToggled(!isToggled);
+	};
 	return (
 		<nav className='bg-nav-gradient'>
-			<div className='container m-auto pt-[10px] pb-1'>
-				<div className='flex justify-center py-1 gap-10'>
-					{/* <Link href={'/football/main'} className='flex items-center'>
+			<div className='container m-auto pt-[10px]  relative'>
+				<div className='flex justify-center pb-1 gap-10'>
+					{isToggled ? <button onClick={onSwitch} className='w-5 h-5 absolute top-[2px] left-4'>
 						<Image
-							className='mr-[9px]'
-							src="/svg/footballIcon.svg"
-							alt="icon football"
-							width={27}
-							height={28}
-						/>
-						<p className='text-black-text font-semibold'>Футбол</p>
-					</Link>
-					<Link href={'/biatlon/main'} className='flex items-center'>
-						<Image
-							className='mr-[9px]'
-							src="/svg/biatlonIcon.svg"
-							alt="icon football"
-							width={30}
-							height={30}
-						/>
-						<p className='text-black-text font-semibold'>Біатлон</p>
-					</Link> */}
-					<Link href={'/sky_racing/main'} className='flex items-center'>
-						<Image
-							className='mr-[9px]'
-							src="/svg/skyRacingIcon.svg"
+							src="/svg/double-arrow-left.svg"
 							alt="icon football"
 							width={39}
 							height={29}
 						/>
-						<p className='text-black-text font-semibold'>Лижні гонки</p>
-					</Link>
-					<Link href={'/alpine_skiing/main'} className='flex items-center'>
-						<Image
-							className='mr-[9px]'
-							src="/svg/alpineSkiingIcon.svg"
-							alt="icon football"
-							width={30}
-							height={32}
-						/>
-						<p className='text-black-text font-semibold'>Гірські лижі</p>
-					</Link>
+					</button> :
+						<button onClick={onSwitch} className='w-5 h-5 absolute top-[2px] right-4'>
+							<Image
+								src="/svg/double-arrow-right.svg"
+								alt="icon football"
+								width={39}
+								height={29}
+							/>
+						</button>}
+					{!isToggled ?
+						<>	<Link href={'/football/main'} className='flex items-center'>
+							<div className='flex h-9 w-9 items-center  justify-center'>
+								<Image
+									className='mr-[9px]'
+									src="/svg/footballIcon.svg"
+									alt="icon football"
+									width={27}
+									height={28}
+								/>
+							</div>
+							<p className='text-black-text font-semibold hover:text-button-hover active:text-button-hover'>Футбол</p>
+						</Link>
+							<Link href={'/biatlon/main'} className='flex items-center'>
+								<div className='flex h-9 w-9 items-center  justify-center'>
+									<Image
+										className='mr-[9px]'
+										src="/svg/biatlonIcon.svg"
+										alt="icon football"
+										width={30}
+										height={30}
+									/>
+								</div>
+								<p className='text-black-text font-semibold hover:text-button-hover active:text-button-hover'>Біатлон</p>
+							</Link>
+						</> : <>
+							<Link href={'/sky_racing/main'} className='flex items-center'>
+								<div className='flex h-9 w-10 items-center  justify-center'>
+									<Image
+										className='mr-[9px]'
+										src="/svg/skyRacingIcon.svg"
+										alt="icon football"
+										width={39}
+										height={29}
+									/>
+								</div>
+								<p className='text-black-text font-semibold hover:text-button-hover active:text-button-hover'>Лижні гонки</p>
+							</Link>
+							<Link href={'/alpine_skiing/main'} className='flex items-center'>
+								<div className='flex h-9 w-9 items-center  justify-center'>
+									<Image
+										className='mr-[9px]'
+										src="/svg/alpineSkiingIcon.svg"
+										alt="icon football"
+										width={30}
+										height={32}
+									/>
+								</div>
+								<p className='text-black-text font-semibold hover:text-button-hover  active:text-button-hover'>Гірські лижі</p>
+							</Link>
+						</>}
 				</div>
 			</div>
 		</nav>
