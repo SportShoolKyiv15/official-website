55b6c7036b4d6b5bfa317be6135e7c9255640c07
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-03-17 23:53:04 +0200

    Add tablet properties in Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -30,8 +30,8 @@ const Nav: React.FC = () => {
 
 	return (
 		<nav className='bg-nav-gradient'>
-			<div className='container m-auto pt-[10px] pb-1 relative'>
-				<div className='flex justify-center gap-10'>
+			<div className='container m-auto pt-[10px] md:pt-[6px] pb-1 md:pb-[6px] relative'>
+				<div className='md:hidden flex justify-center gap-10'>
 					{isToggled ?
 						<button onClick={onSwitch} className='w-5 h-5 absolute top-[2px] left-4'>
 							<Image
@@ -100,6 +100,56 @@ const Nav: React.FC = () => {
 								<p className={`${sport === 'alpine_skiing' ? 'text-button-hover' : 'text-black-text'} font-semibold hover:text-button-hover`}>Гірські лижі</p>
 							</Link>
 						</>}
+				</div>
+				<div className='hidden md:flex justify-between gap-10 px-[14px]'>
+					<Link href={'/football/main'} className='flex items-center'>
+						<div className='flex w-9 items-center  justify-center'>
+							<Image
+								className='mr-[6px]'
+								src="/svg/iconFootball.svg"
+								alt="icon football"
+								width={40}
+								height={40}
+							/>
+						</div>
+						<p className={`${sport === 'football' ? 'text-button-hover' : 'text-black-text'} font-semibold hover:text-button-hover`}>Футбол</p>
+					</Link>
+					<Link href={'/biatlon/main'} className='flex items-center'>
+						<div className='flex w-9 items-center  justify-center'>
+							<Image
+								className='mr-[6px]'
+								src="/svg/iconBiatlon.svg"
+								alt="icon football"
+								width={40}
+								height={49}
+							/>
+						</div>
+						<p className={`${sport === 'biatlon' ? 'text-button-hover' : 'text-black-text'} font-semibold hover:text-button-hover`}>Біатлон</p>
+					</Link>
+					<Link href={'/sky_racing/main'} className='flex items-center'>
+						<div className='flex w-10 items-center  justify-center'>
+							<Image
+								className='mr-[6px]'
+								src="/svg/iconSkyRacing.svg"
+								alt="icon football"
+								width={49}
+								height={40}
+							/>
+						</div>
+						<p className={`${sport === 'sky_racing' ? 'text-button-hover' : 'text-black-text'} font-semibold hover:text-button-hover`}>Лижні гонки</p>
+					</Link>
+					<Link href={'/alpine_skiing/main'} className='flex items-center'>
+						<div className='flex w-9 items-center  justify-center'>
+							<Image
+								className='mr-[6px]'
+								src="/svg/iconAlpineSkiing.svg"
+								alt="icon football"
+								width={40}
+								height={40}
+							/>
+						</div>
+						<p className={`${sport === 'alpine_skiing' ? 'text-button-hover' : 'text-black-text'} font-semibold hover:text-button-hover`}>Гірські лижі</p>
+					</Link>
 				</div>
 			</div>
 		</nav>
