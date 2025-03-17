50943ae345851abb2b675170609eeb1ff6c7970b
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-03-17 23:34:25 +0200

    Add tablet properties in Header and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,54 +7,82 @@ import ContactButton from './buttons/ContactButton';
 const Footer: React.FC = () => {
 	return (
 		<footer className='bg-black flex justify-center'>
-			<div className='container pt-[20px] pb-2'>
-				<div className='flex justify-between items-center mb-[58px]'>
-					<div className='flex items-start'>
-						<Image
-							className="mr-[6px]"
-							src="/svg/iconLocation.svg"
-							alt="icon location"
-							width={24}
-							height={24}
-						/>
-						<div className='flex flex-col gap-[6px] font-display text-sm'>
-							<p>
-								<span className='text-base font-medium'>Київ, </span>
-								Голосіївский район,
-							</p>
-							<p>вул. Героїв Маріуполя 7а</p>
-							<p>Тел.:<span>(044) 250-10-33</span></p>
+			<div className='container pt-5 md:pt-10 pb-2 md:pb-[17px]'>
+				<div className='md:flex md:items-center md:mb-[7px] justify-between'>
+					<div className='flex justify-between items-center mb-[58px] md:mb-0'>
+						<div className='flex items-start'>
+							<Image
+								className="mr-[6px]"
+								src="/svg/iconLocation.svg"
+								alt="icon location"
+								width={24}
+								height={24}
+							/>
+							<div className='flex flex-col gap-[6px] font-display text-sm'>
+								<p>
+									<span className='text-base font-medium'>Київ, </span>
+									Голосіївский район,
+								</p>
+								<p>вул. Героїв Маріуполя 7а</p>
+								<p>Тел.:<span>(044) 250-10-33</span></p>
+							</div>
 						</div>
+						<Link href={'/'} className='md:hidden'>
+							<Image
+								src="/svg/logoFooter.svg"
+								alt="logo"
+								width={107}
+								height={93}
+							/>
+						</Link>
 					</div>
-					<Link href={'/'}>
+					<Link href={'/'} className='hidden md:block'>
 						<Image
 							src="/svg/logoFooter.svg"
 							alt="logo"
-							width={107}
-							height={93}
-						/></Link>
+							width={97}
+							height={83}
+						/>
+					</Link>
+					<div className='flex md:block justify-between mb-8'>
+						<div className='flex flex-col gap-2 font-display text-sm'>
+							<p>Залишились питання?</p>
+							<ContactButton />
+						</div>
+						<div className='flex md:justify-end gap-[10px]'>
+							<Image
+								className="md:hidden"
+								src="/svg/iconInstagram.svg"
+								alt="logo"
+								width={32}
+								height={32}
+							/>
+							<Image
+								className="md:hidden"
+								src="/svg/iconFacebook.svg"
+								alt="logo"
+								width={32}
+								height={32}
+							/>
+						</div>
+					</div>
+
 				</div>
-				<div className='flex justify-between mb-8'>
-					<div className='flex flex-col gap-2 font-display text-sm'>
-						<p>Залишились питання?</p>
-						<ContactButton />
-					</div>
-					<div className='flex gap-[10px]'>
-						<Image
-							className=""
-							src="/svg/iconInstagram.svg"
-							alt="logo"
-							width={26}
-							height={26}
-						/>
-						<Image
-							className=""
-							src="/svg/iconFacebook.svg"
-							alt="logo"
-							width={26}
-							height={26}
-						/>
-					</div>
+				<div className='flex md:justify-end md:mb-8 gap-[10px]'>
+					<Image
+						className="hidden md:block"
+						src="/svg/iconInstagram.svg"
+						alt="logo"
+						width={28}
+						height={28}
+					/>
+					<Image
+						className="hidden md:block"
+						src="/svg/iconFacebook.svg"
+						alt="logo"
+						width={28}
+						height={28}
+					/>
 				</div>
 				<div className='flex flex-col items-center gap-1 text-xs'>
 					<p>© 2024, Всі права захищенні</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,8 +8,9 @@ const Header: React.FC = () => {
 			<div className='container flex justify-between items-center pt-[6px] md:pt-[11px] pb-[7px] md:pb-[11px]'>
 				<Link href={'/'} className='flex items-center'>
 					<Image src='/svg/logoHeader.svg' alt='Logo' width={60} height={51} className='md:hidden mr-[5px]' />
-					<Image src='/svg/logoHeader.svg' alt='Logo' width={82} height={70} className='hidden md:block mr-[5px]' />
-					<p className='text-lg font-ermilov font-bold leading-[110%]'>КДЮСШ</p>
+					<Image src='/svg/logoHeader.svg' alt='Logo' width={82} height={70} className='hidden md:block mr-[10px]' />
+					<p className='md:hidden text-lg font-ermilov font-bold leading-[110%]'>КДЮСШ</p>
+					<p className='hidden md:block md:w-[285px] text-lg font-ermilov font-bold leading-[110%]'>Комплексна дитяча юнацька спортивна школа 15</p>
 				</Link>
 				<button className='md:self-start'>
 					<Image
