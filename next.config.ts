fd886c6f2062493dcd6334d7600cc7ba885e72e1
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-03-30 16:05:18 +0300

    new deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
 	output: "standalone",
 	reactStrictMode: true,
-	swcMinify: true,
 	experimental: {
 		serverActions: {
 			allowedOrigins: ["http://localhost:3000", "localhost:3000"],
