5e7942c2da372730da12256b892d87bad7d38766
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-04-02 22:08:08 +0300

    add swipe 358

--- src/components/SwiperCard.tsx
+++ src/components/SwiperCard.tsx
@@ -9,7 +9,7 @@ const SwiperCard: FC<Props> = ({ children, idx }) => {
 	return (
 		<li
 			key={idx}
-			className="flex flex-col gap-[22px] min-w-[345px] md:min-w-[328px] lg:min-w-[416px] h-[320px] md:h-[292px] lg:h-[368px] rounded-[4px] items-center relative"
+			className="flex flex-col gap-[22px] min-w-[336px] sm:min-w-[345px] md:min-w-[328px] lg:min-w-[416px] h-[320px] md:h-[292px] lg:h-[368px] rounded-[4px] items-center relative"
 		>
 			{children}
 		</li>
