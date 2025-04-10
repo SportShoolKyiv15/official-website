b5b8548a6c822874c24df407477cf576d3ad66a1
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-04-10 20:27:21 +0300

    add adress to a map

--- src/app/sky_racing/main/page.tsx
+++ src/app/sky_racing/main/page.tsx
@@ -8,6 +8,7 @@ import SportPageHero from '@/components/SportPageHero';
 import SportFacility from '@/components/SportFacility';
 import Title from '@/components/Title';
 import MyGoogleMap from '@/components/MyGoogleMap';
+import Image from 'next/image';
 
 const SkyRacingMainPage: FC = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
@@ -39,14 +40,34 @@ const SkyRacingMainPage: FC = () => {
 		<section className="flex flex-col justify-center gap-[50px] md:gap-[80px] items-center pt-[22px]">
 			<SportPageHero bgUrl="bg-[url('/img/skyRacingHeroPageImg.jpg')]" toggleModal={toggleModal} directionImage='rotate-y-180' />
 			<div>
-				<div className="mb-5 md:mb-6">
+				<div className="mb-5 md:mb-6 pl-[10px] sm:pl-[15px] md:pl-0">
 					<Title type="section-subtitle">Лижна база</Title>
 				</div>
 				<SportFacility bgUrl="bg-[url('/img/sportFacilityImg2.jpg')]" />
 			</div>
 			<div>
-				<div className="mb-5 md:mb-9 lg:mb-8 md:text-center">
+				<div className="mb-5 md:mb-9 lg:mb-8 pl-[10px] sm:pl-[15px] md:pl-0 md:text-center">
 					<Title type="section-subtitle">Локація</Title>
+				</div>
+				<div className='lg:flex lg:justify-start lg:items-center lg:mb-5'>
+					<div className='flex justify-center items-ctnter mb-[6px] lg:mb-0 lg:mr-[6px]'>
+						<Image
+							className="md:hidden mr-[2px]"
+							src="/svg/iconLocation.svg"
+							alt="icon location"
+							width={22}
+							height={22}
+						/>
+						<Image
+							className="hidden md:block mr-[2px] lg:mr-2"
+							src="/svg/iconLocation.svg"
+							alt="icon location"
+							width={26}
+							height={26}
+						/>
+						<p className='font-display text-lg font-semibold md:font-medium'>Київ,</p>
+					</div>
+					<p className='mb-[14px] md:mb-6 lg:mb-0 text-center font-display text-sm md:text-lg font-semibold md:font-normal leading-[150%] md:leading-[100%]'>Голосіївский район, вул. Ягідна 2</p>
 				</div>
 				<div className='min-w-[300px] xs:w-[356px] sm:w-[375px] md:w-[680] lg:w-[1296px] h-[187px] md:h-[209px] lg:h-[332px]'>
 					<MyGoogleMap sport='sky' />
