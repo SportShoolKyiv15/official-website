5b0862fb7e5954caa7436930e7beb649e040d9b8
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-04-10 20:35:00 +0300

    add adress for all sport pages

--- src/app/football/main/page.tsx
+++ src/app/football/main/page.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from 'react';
 import { useState } from 'react';
+import Image from 'next/image';
 
 import ModalEnroll from '@/components/modals/ModalEnroll';
 import SportPageHero from '@/components/SportPageHero';
@@ -47,6 +48,26 @@ const FootballMainPage: FC = () => {
 			<div>
 				<div className="mb-5 md:mb-9 lg:mb-8 pl-[10px] sm:pl-[15px] md:pl-0 md:text-center">
 					<Title type="section-subtitle">Локація</Title>
+				</div>
+				<div className='lg:flex lg:justify-start lg:items-center lg:mb-5'>
+					<div className='flex justify-center items-ctnter mb-[6px] lg:mb-0 lg:mr-[6px]'>
+						<Image
+							className="md:hidden mr-[2px]"
+							src="/svg/iconLocation.svg"
+							alt="icon location"
+							width={22}
+							height={22}
+						/>
+						<Image
+							className="hidden md:block mr-[2px] lg:mr-2"
+							src="/svg/iconLocation.svg"
+							alt="icon location"
+							width={26}
+							height={26}
+						/>
+						<p className='font-display text-lg font-semibold md:font-medium'>Київ,</p>
+					</div>
+					<p className='mb-[14px] md:mb-6 lg:mb-0 text-center font-display text-sm md:text-lg font-semibold md:font-normal leading-[150%] md:leading-[100%]'>Голосіївский район, вул. Героїв Маріуполя 7а</p>
 				</div>
 				<div className='min-w-[300px] xs:w-[356px] sm:w-[375px] md:w-[680] lg:w-[1296px] h-[187px] md:h-[209px] lg:h-[332px]'>
 					<MyGoogleMap sport='football' />
