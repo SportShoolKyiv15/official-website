b860b04545f60577a3045f6f645fdc55ad430869
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-04-28 14:41:15 +0300

    modify Header and add prefixes

---
+++ src/contexts/ScrollContext.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from 'react';
+
+type ScrollContextType = {
+	hideHeader: boolean;
+	atTop: boolean;
+}
+
+const ScrollContext = createContext<ScrollContextType>({ hideHeader: false, atTop: true });
+
+export const ScrollProvider = ({ children }: { children: React.ReactNode }) => {
+	const [hideHeader, setHideHeader] = useState(false);
+	const [lastScrollTop, setLastScrollTop] = useState(0);
+	const [atTop, setAtTop] = useState(true);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			const currentScrollTop = window.scrollY;
+			// const windowHeight = window.innerHeight;
+			// const fullHeight = document.body.scrollHeight;
+
+			// const isBottom = currentScrollTop + windowHeight >= fullHeight - 10; // -10 для невеликого запасу
+			const isTop = currentScrollTop < 50;
+
+			setAtTop(isTop);
+
+			if (isTop) {
+				setHideHeader(false);
+			} else {
+				setHideHeader(true);
+			}
+
+			setLastScrollTop(currentScrollTop);
+		};
+
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, [lastScrollTop]);
+
+	return (
+		<ScrollContext.Provider value={{ hideHeader, atTop }}>
+			{children}
+		</ScrollContext.Provider>
+	);
+};
+
+export const useScroll = () => useContext(ScrollContext);
