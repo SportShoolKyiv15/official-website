9a2774ab415157f584132869a084b10a540cc6e9
SportSchoolKyiv15 <sportschoolkyiv15@gmail.com>
2025-04-30 23:23:11 +0300

    fix 2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { useNav } from '../contexts/NavContext';
 import ContactButton from './buttons/ContactButton';
 import FeedbackModal from './modals/FeedbackModal';
 
